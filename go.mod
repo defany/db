f9fcbe0604368a1c5626377491847cac12161012
defany <dimamileris2006@gmail.com>
2025-06-06 20:00:55 +0100

    changed put and put batch methods for river queue, now they return id of inserted jobs

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gookit/goutil v0.6.18 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
--- pkg/postgres/queue/worker.go
+++ pkg/postgres/queue/worker.go
@@ -5,13 +5,15 @@ import (
 
 	"github.com/defany/db/pkg/postgres"
 	"github.com/defany/slogger/pkg/logger/sl"
+	"github.com/gookit/goutil/arrutil"
 	"github.com/jackc/pgx/v5"
 	"github.com/riverqueue/river"
+	"github.com/riverqueue/river/rivertype"
 )
 
 type Worker[T river.JobArgs] interface {
-	Put(ctx context.Context, args T) error
-	PutBatch(ctx context.Context, args ...T) error
+	Put(ctx context.Context, args T) (int64, error)
+	PutBatch(ctx context.Context, args ...T) ([]int64, error)
 }
 
 type Repository[T river.JobArgs] struct {
@@ -22,26 +24,26 @@ func New[T river.JobArgs](river *river.Client[pgx.Tx]) *Repository[T] {
 	return &Repository[T]{river: river}
 }
 
-func (r *Repository[T]) Put(ctx context.Context, args T) error {
+func (r *Repository[T]) Put(ctx context.Context, args T) (int64, error) {
 	tx, ok := postgres.ExtractTX(ctx)
 	if ok {
-		_, err := r.river.InsertTx(ctx, tx, args, nil)
+		out, err := r.river.InsertTx(ctx, tx, args, nil)
 		if err != nil {
-			return err
+			return 0, err
 		}
 
-		return nil
+		return out.Job.ID, nil
 	}
 
-	_, err := r.river.Insert(ctx, args, nil)
+	out, err := r.river.Insert(ctx, args, nil)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
-	return nil
+	return out.Job.ID, nil
 }
 
-func (r *Repository[T]) PutBatch(ctx context.Context, args ...T) error {
+func (r *Repository[T]) PutBatch(ctx context.Context, args ...T) ([]int64, error) {
 	op := sl.FnName()
 
 	insertParams := make([]river.InsertManyParams, 0, len(args))
@@ -53,18 +55,26 @@ func (r *Repository[T]) PutBatch(ctx context.Context, args ...T) error {
 
 	tx, ok := postgres.ExtractTX(ctx)
 	if ok {
-		_, err := r.river.InsertManyFastTx(ctx, tx, insertParams)
+		out, err := r.river.InsertManyTx(ctx, tx, insertParams)
 		if err != nil {
-			return sl.Err(op, err)
+			return nil, sl.Err(op, err)
 		}
 
-		return nil
+		jobIds := arrutil.Map(out, func(input *rivertype.JobInsertResult) (target int64, find bool) {
+			return input.Job.ID, true
+		})
+
+		return jobIds, nil
 	}
 
-	_, err := r.river.InsertManyFast(ctx, insertParams)
+	out, err := r.river.InsertMany(ctx, insertParams)
 	if err != nil {
-		return sl.Err(op, err)
+		return nil, sl.Err(op, err)
 	}
 
-	return nil
+	jobIds := arrutil.Map(out, func(input *rivertype.JobInsertResult) (target int64, find bool) {
+		return input.Job.ID, true
+	})
+
+	return jobIds, nil
 }
