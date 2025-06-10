c906efb691ec005f3ba2129fd15583a60847c872
defany <dimamileris2006@gmail.com>
2025-06-10 16:13:30 +0100

    fts

--- pkg/postgres/connect.go
+++ pkg/postgres/connect.go
@@ -16,15 +16,22 @@ import (
 const (
 	defaultMaxConnAttempts = 3
 	defaultRetryConnDelay  = time.Second
+	defaultAcquireTimeout  = 5 * time.Second
 )
 
 type Config struct {
-	Username   string
-	Password   string
-	Host       string
-	Port       string
-	Database   string
-	ConnAmount int32
+	Username string
+	Password string
+	Host     string
+	Port     string
+	Database string
+
+	ConnAmount        *int32
+	MinConnAmount     *int32
+	MaxConnIdleTime   *time.Duration
+	MaxConnLifetime   *time.Duration
+	HealthCheckPeriod *time.Duration
+	AcquireTimeout    time.Duration
 
 	maxConnAttempts int
 	retryConnDelay  time.Duration
@@ -32,7 +39,7 @@ type Config struct {
 	tracer pgx.QueryTracer
 }
 
-func NewConfig(username string, password string, host string, port string, database string) *Config {
+func NewConfig(username, password, host, port, database string) *Config {
 	return &Config{
 		Username: username,
 		Password: password,
@@ -42,7 +49,54 @@ func NewConfig(username string, password string, host string, port string, datab
 
 		maxConnAttempts: defaultMaxConnAttempts,
 		retryConnDelay:  defaultRetryConnDelay,
+
+		AcquireTimeout: defaultAcquireTimeout,
 	}
+}
+
+func (c *Config) WithConnAmount(amount int32) *Config {
+	c.ConnAmount = &amount
+	return c
+}
+
+func (c *Config) WithMinConnAmount(amount int32) *Config {
+	c.MinConnAmount = &amount
+	return c
+}
+
+func (c *Config) WithMaxConnIdleTime(d time.Duration) *Config {
+	c.MaxConnIdleTime = &d
+	return c
+}
+
+func (c *Config) WithMaxConnLifetime(d time.Duration) *Config {
+	c.MaxConnLifetime = &d
+	return c
+}
+
+func (c *Config) WithHealthCheckPeriod(d time.Duration) *Config {
+	c.HealthCheckPeriod = &d
+	return c
+}
+
+func (c *Config) WithAcquireTimeout(d time.Duration) *Config {
+	c.AcquireTimeout = &d
+	return c
+}
+
+func (c *Config) WithMaxConnAttempts(attempts int) *Config {
+	c.maxConnAttempts = attempts
+	return c
+}
+
+func (c *Config) WithRetryConnDelay(delay time.Duration) *Config {
+	c.retryConnDelay = delay
+	return c
+}
+
+func (c *Config) WithTracer(tracer pgx.QueryTracer) *Config {
+	c.tracer = tracer
+	return c
 }
 
 func (c *Config) dsn() string {
@@ -53,58 +107,51 @@ func (c *Config) dsn() string {
 	)
 }
 
-func (c *Config) WithConnAmount(amount int32) *Config {
-	c.ConnAmount = amount
-
-	return c
-}
-
-func (c *Config) WithMaxConnAttempts(attempts int) *Config {
-	c.maxConnAttempts = attempts
-
-	return c
-}
-
-func (c *Config) WithRetryConnDelay(delay time.Duration) *Config {
-	c.retryConnDelay = delay
-
-	return c
-}
-
-func (c *Config) WithTracer(tracer pgx.QueryTracer) *Config {
-	c.tracer = tracer
-
-	return c
-}
-
+// NewClient создает и конфигурирует пул соединений.
 func NewClient(ctx context.Context, log *slog.Logger, cfg *Config) (pool *pgxpool.Pool, err error) {
 	dsn := cfg.dsn()
 
 	err = retry.WithAttempts(cfg.maxConnAttempts, cfg.retryConnDelay, func() error {
 		log.Info("connecting to postgresql database...")
 
-		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		connectCtx, cancel := context.WithTimeout(ctx, cfg.AcquireTimeout)
 		defer cancel()
 
 		pgxCfg, err := pgxpool.ParseConfig(dsn)
 		if err != nil {
 			log.Error("Unable to parse configs", sl.ErrAttr(err))
-
 			return err
+		}
+
+		if cfg.ConnAmount != nil {
+			pgxCfg.MaxConns = *cfg.ConnAmount
+		}
+
+		if pgxCfg.MinConns != nil {
+			pgxCfg.MinConns = *cfg.MinConnAmount
+		}
+
+		if cfg.MaxConnIdleTime != nil {
+			pgxCfg.MaxConnIdleTime = *cfg.MaxConnIdleTime
+		}
+
+		if cfg.MaxConnLifetime != nil {
+			pgxCfg.MaxConnLifetime = *cfg.MaxConnLifetime
+		}
+
+		if cfg.HealthCheckPeriod != nil {
+			pgxCfg.HealthCheckPeriod = *cfg.HealthCheckPeriod
 		}
 
 		pgxCfg.ConnConfig.Tracer = cfg.tracer
 
-		pgxCfg.MaxConns = cfg.ConnAmount
-
-		pool, err = pgxpool.NewWithConfig(ctx, pgxCfg)
+		pool, err = pgxpool.NewWithConfig(connectCtx, pgxCfg)
 		if err != nil {
 			log.Error("failed to connect to postgres...", sl.ErrAttr(err))
-
 			return err
 		}
 
-		err = pool.Ping(context.Background())
+		err = pool.Ping(ctx)
 		if err != nil {
 			log.Error("ping to postgres failed...", sl.ErrAttr(err))
 		}
@@ -114,11 +161,9 @@ func NewClient(ctx context.Context, log *slog.Logger, cfg *Config) (pool *pgxpoo
 
 	if err != nil {
 		log.Error("all attempts are exceeded. unable to connect to postgres database")
-
 		return nil, err
 	}
 
 	log.Info("connected to postgresql")
-
-	return
+	return pool, nil
 }
