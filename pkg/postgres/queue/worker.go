8f752a7fd2e8246d51755d2a4ff4691f47b90e1b
defany <dimamileris2006@gmail.com>
2025-06-14 20:27:04 +0100

    added methods with options

--- pkg/postgres/queue/worker.go
+++ pkg/postgres/queue/worker.go
@@ -2,6 +2,7 @@ package queue
 
 import (
 	"context"
+	"time"
 
 	"github.com/defany/db/pkg/postgres"
 	slerr "github.com/defany/slogger/pkg/err"
@@ -12,9 +13,15 @@ import (
 	"github.com/riverqueue/river/rivertype"
 )
 
+type Options struct {
+	ScheduledAt time.Time
+}
+
 type Worker[T river.JobArgs] interface {
 	Put(ctx context.Context, args T) (int64, error)
 	PutBatch(ctx context.Context, args ...T) ([]int64, error)
+	PutWithOpts(ctx context.Context, options Options, args T) (int64, error)
+	PutBatchWithOpts(ctx context.Context, options Options, args ...T) ([]int64, error)
 	JobStatuses(ctx context.Context, ids ...int64) ([]JobStatus, error)
 }
 
@@ -52,6 +59,68 @@ func (r *Repository[T]) PutBatch(ctx context.Context, args ...T) ([]int64, error
 	for _, arg := range args {
 		insertParams = append(insertParams, river.InsertManyParams{
 			Args: arg,
+		})
+	}
+
+	tx, ok := postgres.ExtractTX(ctx)
+	if ok {
+		out, err := r.river.InsertManyTx(ctx, tx, insertParams)
+		if err != nil {
+			return nil, sl.Err(op, err)
+		}
+
+		jobIds := arrutil.Map(out, func(input *rivertype.JobInsertResult) (target int64, find bool) {
+			return input.Job.ID, true
+		})
+
+		return jobIds, nil
+	}
+
+	out, err := r.river.InsertMany(ctx, insertParams)
+	if err != nil {
+		return nil, sl.Err(op, err)
+	}
+
+	jobIds := arrutil.Map(out, func(input *rivertype.JobInsertResult) (target int64, find bool) {
+		return input.Job.ID, true
+	})
+
+	return jobIds, nil
+}
+
+func (r *Repository[T]) PutWithOpts(ctx context.Context, options Options, args T) (int64, error) {
+	tx, ok := postgres.ExtractTX(ctx)
+	if ok {
+		out, err := r.river.InsertTx(ctx, tx, args, &river.InsertOpts{
+			ScheduledAt: options.ScheduledAt,
+		})
+		if err != nil {
+			return 0, err
+		}
+
+		return out.Job.ID, nil
+	}
+
+	out, err := r.river.Insert(ctx, args, &river.InsertOpts{
+		ScheduledAt: options.ScheduledAt,
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	return out.Job.ID, nil
+}
+
+func (r *Repository[T]) PutBatchWithOpts(ctx context.Context, options Options, args ...T) ([]int64, error) {
+	op := sl.FnName()
+
+	insertParams := make([]river.InsertManyParams, 0, len(args))
+	for _, arg := range args {
+		insertParams = append(insertParams, river.InsertManyParams{
+			Args: arg,
+			InsertOpts: &river.InsertOpts{
+				ScheduledAt: options.ScheduledAt,
+			},
 		})
 	}
 
