301235bffc9de0c8e568358812c6d4d2b6b8617e
defany <dmitrii.mileris@gmail.com>
2025-07-27 02:00:03 +0200

    added mockut - package for utils for txman, now it got function for mocking tx execution

---
+++ tx_manager/mockut/tx_exec.go
@@ -0,0 +1,22 @@
+package mockut
+
+import (
+	"context"
+
+	txman "github.com/defany/db/v2/tx_manager"
+	mocktxman "github.com/defany/db/v2/tx_manager/mocks"
+	"github.com/stretchr/testify/mock"
+)
+
+func MockOnTxExecution(tx *mocktxman.MockTxManager) {
+	tx.
+		On("RunWithOpts",
+			mock.IsType(context.Background()),
+			mock.MatchedBy(func(h txman.Handler) bool { return h != nil }),
+			mock.IsType([]txman.TxOption{}),
+		).
+		Return(func(ctx context.Context, h txman.Handler, _ []txman.TxOption) error {
+			return h(ctx)
+		}).
+		Once()
+}
